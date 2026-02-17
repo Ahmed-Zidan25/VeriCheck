0bd2cc213fefc8faf2c8afc5b59a19bfd0bdf6f9
Ahmed Zidan <ahmadibrahim.zidan@gmail.com>
2026-02-18 01:20:08 +0200

    2nd version

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-/* import Header from '@/components/sections/Header' */
 import HeroSection from '@/components/sections/HeroSection'
 import VisionMission from '@/components/sections/VisionMission'
 import ServicesGrid from '@/components/sections/ServicesGrid'
@@ -11,7 +10,6 @@ import Footer from '@/components/sections/Footer'
 export default function Home() {
   return (
     <main className="min-h-screen bg-vericheck-white">
-     {/*  <Header /> */}
       <HeroSection />
       <VisionMission />
       <ServicesGrid />
