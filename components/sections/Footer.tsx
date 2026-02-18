b350c767500aee2508fbffe84c356ed028e8241b
Ahmed Zidan <ahmadibrahim.zidan@gmail.com>
2026-02-18 23:26:09 +0200

    edits

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 import { Facebook, Linkedin, MessageCircle, Mail, Phone, MapPin, CheckCircle2 } from 'lucide-react'
 
 export default function Footer() {
